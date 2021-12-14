4c26d0b531ffd01d4e79848f52142f5daba99495
Mikita <haniks2899@gmail.com>
2021-12-14 16:57:41 +0300

    feat(todo): added todo list

--- scripts/index.js
+++ scripts/index.js
@@ -16,6 +16,14 @@ const header = createHeader()
 const main = createMain()
 
 divTitle.append(title)
+root.append(
+  divTitle,
+  header,
+  main,
+  `All: ${todos.length}`,
+  ` Completed: ${completedTasks}`,
+  todoList
+)
 
 // ELEMENT--------------------------------------------
 function createElement(tag, className, text = "", id = "") {
@@ -61,4 +69,37 @@ function createMain() {
 
   main.append(buttonShowAll, buttonShowCompleted, input)
   return main
+}
+
+// create ToDo List---------------------------------------------------
+function createTodoList(todos) {
+  const list = createElement("div", "d-flex flex-column gap-2")
+  todos.forEach((todo) => {
+    const todoItem = createTodoItem(todo)
+    list.append(todoItem)
+  })
+  return list
+}
+
+// ELEMENT TODO-------------------------------------
+function createTodoItem(todo) {
+  const classInputOn =
+    "d-flex align-items-center gap-1 border border-danger py-2 px-4 rounded-3 bg-danger text-white text-break"
+  const classInputOff =
+    "d-flex align-items-center gap-1 border border-danger py-2 px-4 rounded-3 text-break"
+  let todoItem
+  if (todo.isCompleted) {
+    todoItem = createElement("div", classInputOn, "", todo.id)
+  } else {
+    todoItem = createElement("div", classInputOff, "", todo.id)
+  }
+  const input = createElement("input", "", "", "")
+  input.type = "checkbox"
+  input.checked = todo.isCompleted
+  const text = createElement("span", "flex-grow-1 px-2", todo.text)
+  const buttonDelete = createElement("butoon", "btn btn-close", "", "")
+  buttonDelete.id = todo.id
+  const date = createElement("span", "badge bg-secondary mx-2", todo.date)
+  todoItem.append(input, text, date, buttonDelete)
+  return todoItem
 }
