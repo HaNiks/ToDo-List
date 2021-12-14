57cbc1d7b70ab65a45f43983866d1a09d3fe0ea3
Mikita <haniks2899@gmail.com>
2021-12-14 16:45:13 +0300

    feat(todo): added title

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,16 @@
 const root = document.querySelector("#root")
+
+let todos = []
+let complited = []
+let completedTasks = 0
+
+const divTitle = createElement(
+  "div",
+  "d-flex justify-content-center pb-2 border-bottom border-danger border-2 text-danger",
+  "",
+  ""
+)
+
+const title = createElement("h2", "", "ToDo LIST", "")
+
+divTitle.append(title)
